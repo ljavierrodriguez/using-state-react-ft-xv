daf7acc2646e4442dad1fc368a2209aa57ad4ea2
Luis J. Rodriguez O <ljavierrodriguez@gmail.com>
2022-04-18 16:28:51 +0000

    react events

--- oop.js
+++ oop.js
@@ -32,7 +32,7 @@ class Persona {
     genero;
     peso;
 
-    constructor(nombre, apellido, edad, rut, genero, peso){
+    constructor(nombre, apellido, edad, rut, genero, peso) {
         this.nombre = nombre;
         this.apellido = apellido;
         this.edad = edad;
@@ -41,27 +41,27 @@ class Persona {
         this.peso = peso;
     }
 
-    hablar = function(){
+    hablar = function () {
         console.log('Hablar Persona');
     }
-    correr = function(){}
-    caminar = function(){}
-    nadar = function(){}
+    correr = function () { }
+    caminar = function () { }
+    nadar = function () { }
 }
 
 class Estudiante extends Persona {
     grado;
     facultad;
 
-    constructor(nombre, apellido, edad, rut, genero, peso){
+    constructor(nombre, apellido, edad, rut, genero, peso) {
         super(nombre, apellido, edad, rut, genero, peso);
     }
 
-    estudiar = function(){}
-    hablar = function(){
+    estudiar = function () { }
+    hablar = function () {
         console.log('Hablar Estudiante 1')
     }
-    hablar = function(string){
+    hablar = function (string) {
         console.log('Hablar Estudiante 2')
     }
 }
@@ -82,3 +82,25 @@ console.log(persona);
 let estudiante = new Estudiante("Anyelina", "Paredes", "28", "12345678-9", "Femenino", "85");
 estudiante.hablar();
 console.log(estudiante);
+
+
+
+let regex = /^[a-zA-Z]+$/;
+
+regex.test('abc123'); // false
+
+
+if (!regex.test('abc123')) {
+    //setError({ 'nombre': 'El nombre no puede contener numeros.' });
+}
+
+let nombre = 'abc123';
+
+if(validarSoloLetras(nombre)){
+    console.log({ 'nombre': 'El nombre no puede contener numeros.' });
+    cvc.classList.add('bg-danger');
+}
+
+function validarSoloLetras(nombre){
+    return !regex.test(nombre);
+}
--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -1,84 +1,134 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
-/* 
 const Navbar = (props) => {
-    return (
-        <nav className="nav justify-content-center">
-            {props.links.map(({ label, url }, index) => (
-                <a href={url} className="nav-link" key={index}>
-                    {label}
-                </a>
-            ))}
-        </nav>
-    );
-}; 
-*/
+	/* const [valor, function] = useState(valorInicial) */
+	const [state, setState] = useState({
+		nombre: "",
+		apellido: "Rodriguez",
+	});
 
-class Navbar extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			nombre: "Luis",
-			apellido: "Rodriguez",
-		};
+	const [name, setName] = useState("Ana");
+	const [lastname, setLastname] = useState("Smith");
 
-		/* 		this.saludo = this.saludo.bind(this);
+	const cambiarNombre = (e) => {
+		/* setState({
+            ...state,
+            nombre: "Pedro",
+        }); */
+		/* setState((prevState) => {
+            return { ...prevState, nombre: "Pedro" };
+        }); */
+		setState((prevState) => {
+			return { ...prevState, [e.target.name]: e.target.value };
+		});
+	};
+
+	return (
+		<nav className="nav justify-content-center">
+			{props.links.map(({ label, url }, index) => (
+				<a href={url} className="nav-link" key={index}>
+					{label}
+				</a>
+			))}
+			<a href="" className="nav-link">
+				{state.nombre} {state.apellido}
+			</a>
+			<a href="" className="nav-link">
+				{name} {lastname}
+			</a>
+			<button className="btn btn-success" onClick={cambiarNombre}>
+				Cambiar Nombre
+			</button>
+			{/* <input type="text" onChange={(e) => setName(e.target.value)} />
+			<input type="text" onChange={(e) => setLastname(e.target.value)} /> */}
+
+			<input
+				type="text"
+				onChange={(e) => cambiarNombre(e)}
+				placeholder="Ingrese Nombre"
+				name="nombre"
+				className={state.nombre === "" ? "alert-danger" : ""}
+			/>
+			<input
+				type="text"
+				onChange={(e) =>
+					setState((prevState) => {
+						return {
+							...prevState,
+							[e.target.name]: e.target.value,
+						};
+					})
+				}
+				placeholder="Ingrese Apellido"
+				name="apellido"
+			/>
+		</nav>
+	);
+};
+
+/* class Navbar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            nombre: "Luis",
+            apellido: "Rodriguez",
+        };
+
+        this.saludo = this.saludo.bind(this);
         this.saludo2 = this.saludo2.bind(this);
         this.saludo3 = this.saludo3.bind(this);
-        this.saludo4 = this.saludo4.bind(this); */
-	}
+        this.saludo4 = this.saludo4.bind(this);
+    }
 
-	saludo = () => {
-		this.saludo2();
-		this.saludo3();
-	};
+    saludo = () => {
+        this.saludo2();
+        this.saludo3();
+    };
 
-	saludo2 = () => {
-		console.log(this.props.links[0].label);
-	};
+    saludo2 = () => {
+        console.log(this.props.links[0].label);
+    };
 
-	saludo3 = () => {
-		console.log(this.props);
-	};
+    saludo3 = () => {
+        console.log(this.props);
+    };
 
-	saludo4 = (e) => {
-		console.log(e.target);
-		console.log(this.state);
-		this.setState({ nombre: "Ramon" });
-	};
+    saludo4 = (e) => {
+        console.log(e.target);
+        console.log(this.state);
+        this.setState({ nombre: "Ramon" });
+    };
 
-	render() {
-		return (
-			<>
-				{this.saludo()}
-				<nav className="nav justify-content-center">
-					{this.props.links.map(({ label, url }, index) => (
-						<a href={url} className="nav-link" key={index}>
-							{label}
-						</a>
-					))}
-					<a href="" className="nav-link">
-						{this.state.nombre} {this.state.apellido}
-					</a>
-					{/* <button
-						className="btn btn-success"
-						onClick={this.saludo4}>
-						Cambiar Nombre
-					</button> */}
-					<button
-						className="btn btn-success"
-						onClick={(e) => {
-							this.saludo2(e);
-							this.saludo4(e);
-						}}>
-						Cambiar Nombre
-					</button>
-				</nav>
-			</>
-		);
-	}
-}
+    render() {
+        return (
+            <>
+                {this.saludo()}
+                <nav className="nav justify-content-center">
+                    {this.props.links.map(({ label, url }, index) => (
+                        <a href={url} className="nav-link" key={index}>
+                            {label}
+                        </a>
+                    ))}
+                    <a href="" className="nav-link">
+                        {this.state.nombre} {this.state.apellido}
+                    </a>
+                    <button className="btn btn-success" onClick={this.saludo4}>
+                        Cambiar Nombre
+                    </button>
+                    <button
+                        className="btn btn-success"
+                        onClick={(e) => {
+                            this.saludo2(e);
+                            this.saludo4(e);
+                        }}>
+                        Cambiar Nombre
+                    </button>
+                </nav>
+            </>
+        );
+    }
+} */
 
 Navbar.propTypes = {
 	links: PropTypes.array.isRequired,
